c3944be157dfd6c8238eba0fcb96fcbf52990111
Oleksii <stinovoleksij@gmail.com>
2022-12-17 11:01:29 +0200

    Added a todo list

--- apps/my-app/src/app/app.component.ts
+++ apps/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  todos: Todo[] = [];
+  newTodo: string;
+
+  saveToDo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+      this.newTodo = '';
+    } else {
+      alert('Enter a new toDo');
+    }
+  }
+
+  done(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter((v,i) => {
+      i !== id;
+    });
+  }
 }
